132268bc2bf789f2e90723efba1e004379fbc1f9
KEHUI YAO <ykhray19960527@gmail.com>
2021-03-08 23:34:18 +0800

    add submit2,submit3

--- conda/submit.sh
+++ conda/submit.sh
@@ -36,4 +36,4 @@ conda list
 
 # Modify these lines to run your desired Python script
 python -c "import torch; print(f'PyTorch version: {torch.__version__}'); print(f'CUDA device: {torch.cuda.get_device_name(0)}')"
-python train.py --epochs=100
+python train.py --epochs=100 --save_figure='model.png' 
