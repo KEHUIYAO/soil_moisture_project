36efd7654977be394f9cad139d461a5d4c271fd3
KEHUI YAO <ykhray19960527@gmail.com>
2021-03-15 23:47:24 +0800

    hope this works

--- src/transformer.py
+++ src/transformer.py
@@ -146,8 +146,8 @@ def subsequent_mask(mask):
     subsequent_mask = torch.ones((size,size), device=device) * mask
 
     off_diagonal_mask = torch.diag(torch.ones(size - 1), diagonal=1)
-    off_diagonal_mask.to(device)
-    subsequent_mask.masked_fill_(off_diagonal_mask==1,torch.tensor(0, device = device))
+    off_diagonal_mask = off_diagonal_mask.to(device)
+    subsequent_mask.masked_fill_(off_diagonal_mask==1, 0)
 
     return subsequent_mask.unsqueeze(0)
 
